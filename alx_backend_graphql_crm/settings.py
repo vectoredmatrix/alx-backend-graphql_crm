90aa4740c5464abc251dbc9aeeb538cc5652cfa4
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-10-17 23:49:22 +0100

    started app crm

--- alx_backend_graphql_crm/settings.py
+++ alx_backend_graphql_crm/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django'
+    
 ]
+
+GRAPHENE = {
+    'SCHEMA': 'crm.schema.schema'
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
